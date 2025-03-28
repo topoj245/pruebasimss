304500b02677f7d634a4b970abcec4337be8f4c3
topoj245 <topoj245@gmail.com>
2025-03-28 14:40:59 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -19,25 +19,41 @@ document.getElementById("eye").addEventListener("click", function () {
 }, false);
 
 // Función de login
+
+
 function login() {
-    const username = document.getElementById("txt-input").value;
+    const username = document.getElementById("txt-input").value.toLowerCase();
     const password = document.getElementById("pwd").value;
 
-    // Credenciales (modifica estos valores)
-    const users = {
-        "usuario": { 
-            password: "user123", 
-            link: "https://farfay.maps.arcgis.com/apps/webappviewer/index.html?id=e95dba9036e844719050945756a98cd0" 
-        },
-        "admin": { 
-            password: "admin123", 
-            link: "https://farfay.maps.arcgis.com/apps/webappviewer/index.html?id=477569cd8fd4495c8ef6e0f9883c7e4d" 
-        }
-    };
+    // Lista de usuarios
+    const usuariosAccesoGeneral = [
+        "jose ramon enriquez",
+        "carlos velasco",
+        "carlos segovia",
+        "carlos silva"
+    ];
 
-    if (users[username] && users[username].password === password) {
-        window.location.href = users[username].link; // Redirección
-    } else {
-        alert("Usuario o contraseña incorrectos");
+    const usuariosAuditoria = [
+        "christopher rivera",
+        "alejandro miranda"
+    ];
+
+    // Contraseñas
+    const CONTRASENA_GENERAL = "mapa_123";
+    const CONTRASENA_AUDITORIA = "auditoria456";
+
+    // Verificar acceso y mostrar mensaje personalizado
+    if (usuariosAccesoGeneral.includes(username) && password === CONTRASENA_GENERAL) {
+        const primerNombre = username.split(" ")[0]; // Extrae "josé", "carlos", etc.
+        alert(`Bienvenido, ${primerNombre}! (Acceso General)`); // Mensaje personalizado
+        window.location.href = "https://farfay.maps.arcgis.com/apps/webappviewer/index.html?id=e95dba9036e844719050945756a98cd0" ;
+    } 
+    else if (usuariosAuditoria.includes(username) && password === CONTRASENA_AUDITORIA) {
+        const primerNombre = username.split(" ")[0]; 
+        alert(`Bienvenido, ${primerNombre}! (Acceso Auditoría)`); 
+        window.location.href = "https://farfay.maps.arcgis.com/apps/webappviewer/index.html?id=477569cd8fd4495c8ef6e0f9883c7e4d" ;
+    } 
+    else {
+        alert("Nombre o contraseña incorrectos");
     }
 }
